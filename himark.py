22eb7aa3d3ac1986729a1a6648da23499a5b8c36
Amir Eldor <amir@eize.ninja>
2018-07-28 15:15:14 +0300

    It kinda works, roughly

--- himark.py
+++ himark.py
@@ -1,8 +1,36 @@
+# oh hi mark!
+# hi lisa
+#
+# Get a markdown file via argument or stdin and output to a tempfile. Open with
+# browser. Fun!
+# License: WTFPL
+from argparse import ArgumentParser
 from markdown import markdown
+from tempfile import NamedTemporaryFile
+import webbrowser
 
 
-with open("input.md") as md:
-    html = markdown(md.read())
+def main():
+    parser = ArgumentParser(description="Temporary render markdown files to a "
+                                        "temporary file and open in the (your!) browser")
+    parser.add_argument("filename", help="A markdown file")
+    args = parser.parse_args()
 
-with open("output.html", "w") as out:
-    out.write(html)
+    try:
+        with open(args.filename) as md:
+            html = markdown(md.read())
+
+        tempfile = NamedTemporaryFile(mode="w+", delete=False)
+        tempfile.write(html)
+
+        print("FILE IFLE ILFE", tempfile.name)
+        webbrowser.open("file://{}".format(tempfile.name))
+
+
+    except FileNotFoundError:
+        print('File not found "{filename}"'.format(filename=args.filename))
+
+
+if __name__ == "__main__":
+    main()
+
